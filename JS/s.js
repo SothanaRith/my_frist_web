54814180f17fc2b3a31d9943e7607c246afe9271
SothanaRith <heangsothanarith@gmail.com>
2023-05-25 08:42:55 +0700

    delete

--- JS/s.js
+++ JS/s.js
@@ -17,11 +17,4 @@ $(document).ready(function(){
     }
   $('#heading').empty().append(message + " from");
 });
-let button = document.querySelector("button");
-let text = document.querySelector("textarea");
-button.addEventListener("click", () =>{
-    let utterance = new
-    SpeechSynthesisUtterance(text.value);
-    speechSynthesis.speak(utterance);
-});
 
